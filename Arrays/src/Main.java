1d6324e6c799e216dbb3ab79d14f06913da79557
BryantVaughn <bryant.f.vaughn@gmail.com>
2020-07-27 16:19:23 -0600

    different ways to initialize values in array

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int[] myIntArray = new int[10];
-        myIntArray[5] = 50;
+        // Int array of length 10
+        int[] myIntArray = new int[20]; // { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        for (int i = 0; i < myIntArray.length; i++) {
+            myIntArray[i] = i * 10;
+        }
+        for (int j: myIntArray) {
+            System.out.println("Element " + (j/10) + ", value is " + j);
+        }
     }
 }
